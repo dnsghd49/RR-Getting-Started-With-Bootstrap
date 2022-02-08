e7763ba268b4fc164a196cb8f5d63454f896b8d2
Aaron Lu <aaronnoh0409@gmail.com>
2022-02-07 19:25:45 -0500

    modified package.json to start the app with my version of node, added bootstrap and tested out with a button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <Button variant='danger'>haha</Button>
     </div>
   );
 }
